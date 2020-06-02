ba89c4e8dd3651e4795f16f8d7bf367c33b78204
Alon Zuman <alonzuman7@gmail.com>
2020-06-02 14:11:16 +0300

    m

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,17 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def follow
+    @user = User.find(params[:id])
+    @follow = Follow.new(follower_id: current_user.id, followed_user_id: @user.id)
+    return (redirect_to user_path(@user)) if @follow.save
+  end
+  
+  def unfollow
+    @user = User.find(params[:id])
+    @follow = Follow.where(follower_id: current_user.id)
+    @follow.destroy_all
+    return (redirect_to user_path(@user)) if @follow.destroy_all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments, only: [:new, :create]
   end
+  get '/users/:id/follow', to: 'users#follow', as: 'follow'
+  get '/users/:id/unfollow', to: 'users#unfollow', as: 'unfollow'
   resources :comments, only: [:destroy]
   resources :users, only: [:show]
 end
